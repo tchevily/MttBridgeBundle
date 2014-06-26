4e2c10e5bc6a5fd0a0a888aa09883740d5bd27f2
Rémy Abi-Khalil <remyabi@gmail.com>
2014-06-26 14:50:54 +0200

    Refs #IUS-211 ~ Check function implemented in component.

--- Monitoring/Component.php
+++ Monitoring/Component.php
@@ -12,6 +12,33 @@ class Component extends AbstractComponentMonitor
         parent::__construct();
 
         $this->name = 'TimeTable';
-        $this->state = State::UP;
+    }
+
+    private function checkServices($category)
+    {
+        foreach ($category->getServices() as $service) {
+            if ($service->getState() == State::DOWN) {
+                $this->state = State::DOWN;
+                break;
+            }
+        }
+    }
+
+    private function checkCategories()
+    {
+        foreach ($this->categories as $category) {
+            $this->checkServices($category);
+            if ($this->state == State::DOWN) {
+                break;
+            }
+        }
+    }
+
+    public function check()
+    {
+        parent::check();
+
+        $this->checkCategories();
+        $this->state = ($this->state == State::UNKNOWN) ? State::UP: $this->state;
     }
 }
